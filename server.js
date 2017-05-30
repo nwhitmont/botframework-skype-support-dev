74d39ece0e2f5a27decc4e846bf12ac71c399a49
nwhitmont <v-niwh@microsoft.com>
2017-05-30 08:30:40 -0700

    update media URLs to https to meet Apple’s App Transport Security rules (https://developer.apple.com/news/?id=12212016b)

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ function createAnimationCard(session) {
         .subtitle('Animation Card')
         .image(builder.CardImage.create(session, 'https://docs.botframework.com/en-us/images/faq-overview/botframework_overview_july.png'))
         .media([
-            { url: 'http://i.giphy.com/Ki55RUbOV5njy.gif' }
+            { url: 'https://i.giphy.com/Ki55RUbOV5njy.gif' }
         ]);
 }
 
@@ -148,7 +148,7 @@ function createVideoCard(session) {
         .text('Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation\'s previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.')
         .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg'))
         .media([
-            { url: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }
+            { url: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }
         ])
         .buttons([
             builder.CardAction.openUrl(session, 'https://peach.blender.org/', 'Learn More')
@@ -162,7 +162,7 @@ function createAudioCard(session) {
         .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
         .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'))
         .media([
-            { url: 'http://www.wavlist.com/movies/004/father.wav' }
+            { url: 'https://www.wavlist.com/movies/004/father.wav' }
         ])
         .buttons([
             builder.CardAction.openUrl(session, 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back', 'Read More')
