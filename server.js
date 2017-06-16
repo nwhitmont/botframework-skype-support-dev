285c5480b8f95871a1d2408541cab56cc19a7776
nwhitmont <v-niwh@microsoft.com>
2017-06-16 12:35:06 -0700

    add demo - message with URL

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // This loads the environment variables from the .env file
-//require('dotenv-extended').load();
+// require('dotenv-extended').load();
 
 var builder = require('botbuilder');
 var restify = require('restify');
@@ -11,51 +11,57 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 server.get('/', function (request, response, next) {
-    response.send(200, { status: 'online' });
+    response.send(200, {status: 'online'});
     next();
 });
 
 server.get('/status', function (request, response) {
-    response.send({ status: 'online' });
+    response.send({status: 'online'});
 });
 
 // Create chat bot and listen to messages
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+var connector = new builder.ChatConnector({appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD});
 server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        builder.Prompts.choice(session, 'What card would like to test?', CardNames, {
-            maxRetries: 3,
-            retryPrompt: 'Ooops, what you wrote is not a valid option, please try again'
-        });
+        builder
+            .Prompts
+            .choice(session, 'What card would like to test?', CardNames, {
+                maxRetries: 3,
+                retryPrompt: 'Ooops, what you wrote is not a valid option, please try again'
+            });
     },
     function (session, results) {
 
         // create the card based on selection
         var selectedCardName = results.response.entity;
-        var card = createCard(selectedCardName, session);
 
-        // attach the card to the reply message
-        var msg = new builder.Message(session).addAttachment(card);
-        session.send(msg);
+        if (selectedCardName === MessageWithUrl) {
+            session.send('Message with URL - https://botframework.com');
+            session.endConversation();
+        } else {
+            var card = createCard(selectedCardName, session);
+            // attach the card to the reply message
+            var msg = new builder.Message(session).addAttachment(card);
+            session.send(msg);
+            session.endConversation();
+        }
 
-        session.send("Audio, video card should work now?");
-        session.endConversation();
     }
 ]);
 
+var MessageWithUrl = 'Message with URL';
 var HeroCardName = 'Hero card';
 var ThumbnailCardName = 'Thumbnail card';
 var ReceiptCardName = 'Receipt card';
 var SigninCardName = 'Sign-in card';
-var AnimationCardName = "Animation card";
-var VideoCardName = "Video card";
-var AudioCardName = "Audio card";
+var AnimationCardName = 'Animation card';
+var VideoCardName = 'Video card';
+var AudioCardName = 'Audio card';
+
 var CardNames = [
+    MessageWithUrl,
     HeroCardName,
     ThumbnailCardName,
     ReceiptCardName,
@@ -86,105 +92,160 @@ function createCard(selectedCardName, session) {
     }
 }
 
-function createBasicMessage(session) {
-
-}
+function createBasicMessage(session) {}
 function createHeroCard(session) {
-    return new builder.HeroCard(session)
+    return new builder
+        .HeroCard(session)
         .title('BotFramework Hero Card')
         .subtitle('Your bots — wherever your users are talking')
-        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .text('Build and connect intelligent bots to interact with your users naturally whereve' +
+                'r they are, from text/sms to Skype, Slack, Office 365 mail and other popular ser' +
+                'vices.')
         .images([
-            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+            builder
+                .CardImage
+                .create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbo' +
+                        'tframework_960.jpg')
         ])
         .buttons([
-            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
+            builder
+                .CardAction
+                .openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
         ]);
 }
 
 function createThumbnailCard(session) {
-    return new builder.ThumbnailCard(session)
+    return new builder
+        .ThumbnailCard(session)
         .title('BotFramework Thumbnail Card')
         .subtitle('Your bots — wherever your users are talking')
-        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .text('Build and connect intelligent bots to interact with your users naturally whereve' +
+                'r they are, from text/sms to Skype, Slack, Office 365 mail and other popular ser' +
+                'vices.')
         .images([
-            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+            builder
+                .CardImage
+                .create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbo' +
+                        'tframework_960.jpg')
         ])
         .buttons([
-            builder.CardAction.openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
+            builder
+                .CardAction
+                .openUrl(session, 'https://docs.botframework.com/en-us/', 'Get Started')
         ]);
 }
 
 var order = 1234;
 function createReceiptCard(session) {
-    return new builder.ReceiptCard(session)
+    return new builder
+        .ReceiptCard(session)
         .title('John Doe')
         .facts([
-            builder.Fact.create(session, order++, 'Order Number'),
-            builder.Fact.create(session, 'VISA 5555-****', 'Payment Method')
+            builder
+                .Fact
+                .create(session, order++, 'Order Number'),
+            builder
+                .Fact
+                .create(session, 'VISA 5555-****', 'Payment Method')
         ])
         .items([
-            builder.ReceiptItem.create(session, '$ 38.45', 'Data Transfer')
+            builder
+                .ReceiptItem
+                .create(session, '$ 38.45', 'Data Transfer')
                 .quantity(368)
-                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png')),
-            builder.ReceiptItem.create(session, '$ 45.00', 'App Service')
+                .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.p' +
+                        'ng')),
+            builder
+                .ReceiptItem
+                .create(session, '$ 45.00', 'App Service')
                 .quantity(720)
                 .image(builder.CardImage.create(session, 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png'))
         ])
         .tax('$ 7.50')
         .total('$ 90.95')
         .buttons([
-            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/pricing/', 'More Information')
-                .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/microsoft-azure.png')
+            builder
+                .CardAction
+                .openUrl(session, 'https://azure.microsoft.com/en-us/pricing/', 'More Information')
+                .image('https://raw.githubusercontent.com/amido/azure-vector-icons/master/renders/micros' +
+                        'oft-azure.png')
         ]);
 }
 
 function createSigninCard(session) {
-    return new builder.SigninCard(session)
+    return new builder
+        .SigninCard(session)
         .text('BotFramework Sign-in Card')
         .button('Sign-in', 'https://login.microsoftonline.com');
 }
 
 function createAnimationCard(session) {
-    return new builder.AnimationCard(session)
+    return new builder
+        .AnimationCard(session)
         .title('Microsoft Bot Framework')
         .subtitle('Animation Card')
-        .image(builder.CardImage.create(session, 'https://docs.botframework.com/en-us/images/faq-overview/botframework_overview_july.png'))
+        .image(builder.CardImage.create(session, 'https://docs.botframework.com/en-us/images/faq-overview/botframework_overview_ju' +
+                'ly.png'))
         .media([
-            { url: 'https://i.giphy.com/Ki55RUbOV5njy.gif' }
+            {
+                url: 'https://i.giphy.com/Ki55RUbOV5njy.gif'
+            }
         ]);
 }
 
 function createVideoCard(session) {
-    return new builder.VideoCard(session)
+    return new builder
+        .VideoCard(session)
         .title('Big Buck Bunny')
         .subtitle('by the Blender Institute')
-        .text('Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation\'s previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.')
-        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg'))
+        .text('Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by th' +
+                'e Blender Institute, part of the Blender Foundation. Like the foundation\'s prev' +
+                'ious film Elephants Dream, the film was made using Blender, a free software appl' +
+                'ication for animation made by the same foundation. It was released as an open-so' +
+                'urce film under Creative Commons License Attribution 3.0.')
+        .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_' +
+                'big.jpg/220px-Big_buck_bunny_poster_big.jpg'))
         .media([
-            { url: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4' }
+            {
+                url: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
+            }
         ])
         .buttons([
-            builder.CardAction.openUrl(session, 'https://peach.blender.org/', 'Learn More')
+            builder
+                .CardAction
+                .openUrl(session, 'https://peach.blender.org/', 'Learn More')
         ]);
 }
 
 function createAudioCard(session) {
-    return new builder.AudioCard(session)
+    return new builder
+        .AudioCard(session)
         .title('I am your father')
         .subtitle('Star Wars: Episode V - The Empire Strikes Back')
-        .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
+        .text('The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes' +
+                ' Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leig' +
+                'h Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing t' +
+                'he film\'s story and serving as executive producer. The second installment in th' +
+                'e original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. a' +
+                'nd stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony ' +
+                'Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.')
         .image(builder.CardImage.create(session, 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'))
         .media([
-            { url: 'https://www.wavlist.com/movies/004/father.wav' }
+            {
+                url: 'https://www.wavlist.com/movies/004/father.wav'
+            }
         ])
         .buttons([
-            builder.CardAction.openUrl(session, 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back', 'Read More')
+            builder
+                .CardAction
+                .openUrl(session, 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back', 'Read More')
         ]);
 }
 
-bot.dialog('exit', function (session) {
-    session.endConversation('Goodbye!');
-}).triggerAction({ matches: /(quit|exit)/i });
+bot
+    .dialog('exit', function (session) {
+        session.endConversation('Goodbye!');
+    })
+    .triggerAction({matches: /(quit|exit)/i});
 
 // END OF LINE
